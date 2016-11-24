e74e91beaf94f43ab41e3564a4fb08f261685eba
daniel2 <dw@dw.local>
2016-11-24 15:06:14 +1100

    final

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # route53-export-zones
+Requires the use of cli53
+Install it from here: https://github.com/barnybug/cli53
--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # Amazon Route53 to BIND export script
+# Daniel Wagner 2016
+
+# Check cli53 is installed
+command cli53 -v >/dev/null 2>&1 || { echo >&2 "I require cli53 but it's not installed.  Aborting."; exit 1; }
 
 # Setup
 touch filelocation
