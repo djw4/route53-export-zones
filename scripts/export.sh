769947155f8a349d6923365851b04b653493fe8a
daniel2 <dw@dw.local>
2016-11-24 14:36:24 +1100

    third commit

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for LINE in $(cat route53-domains.csv); do touch zones/$LINE.db && cli53 export --full $LINE > zones/$LINE.db; done
+for LINE in $(cat $domainlist); do touch zones/$LINE.db && cli53 export --full $LINE > zones/$LINE.db; done
